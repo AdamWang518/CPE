c6205f0fd93f9725e725e43444b623d17320df45
AdamWang518 <dodoga518@gmail.com>
2024-07-30 20:55:59 +0800

    string

--- 1-5.cpp
+++ 1-5.cpp
@@ -1,29 +1,34 @@
-#include <iostream>
-#include <vector>
-#include <string>
 #include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
-int main() {
+int main()
+{
     vector<string> lines;
     string line;
     size_t max_len = 0;
 
     // 讀取所有行
-    while (getline(cin, line)) {
+    while (getline(cin, line))
+    {
         lines.push_back(line);
         max_len = max(max_len, line.length());
     }
 
     // 用空格填充每一行，使得所有行的長度一致
-    for (auto& l : lines) {
+    for (string &l : lines)
+    {
         l.resize(max_len, ' ');
     }
 
-    // 旋轉句子
-    for (size_t i = 0; i < max_len; ++i) {
-        for (int j = lines.size() - 1; j >= 0; --j) {
+    // 旋轉句子，因為要轉90度所以從最後的row開始
+    for (size_t i = 0; i < max_len; ++i)
+    {
+        for (int j = lines.size() - 1; j >= 0; --j)
+        {
             cout << lines[j][i];
         }
         cout << endl;
