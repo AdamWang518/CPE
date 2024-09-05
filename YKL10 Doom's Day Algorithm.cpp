f35d525b3a09689ddf86a82a9bb18c2bc4df5cba
AdamWang518 <dodoga518@gmail.com>
2024-09-05 19:58:39 +0800

    10

---
+++ YKL10 Doom's Day Algorithm.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+// 星期對應的名稱
+string daysOfWeek[] = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+
+// Doomsday 日期（2011 年非閏年）
+int doomsdays[] = {0, 10, 21, 0, 4, 9, 6, 11, 8, 5, 10, 7, 12};
+
+// 計算日期對應的星期幾
+string getDayOfWeek(int month, int day)
+{
+    // 找到該月份的 Doomsday
+    int doomsday = doomsdays[month];
+    // 計算日期與該月份 Doomsday 的天數差
+    int difference = day - doomsday;
+    // Doomsday 是星期一，所以從星期一開始計算
+    int dayOfWeek = (0 + difference % 7 + 7) % 7; // 保證結果在 [0, 6] 範圍內，，處理負數
+    return daysOfWeek[dayOfWeek];
+}
+
+int main()
+{
+    int t;
+    cin >> t; // 測試用例數量
+    while (t--)
+    {
+        int month, day;
+        cin >> month >> day;
+        cout << getDayOfWeek(month, day) << endl;
+    }
+    return 0;
+}
