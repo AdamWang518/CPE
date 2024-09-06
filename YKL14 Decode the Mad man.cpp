53c0f05446cbc54178c631931a43eb5865a0c9ec
AdamWang518 <dodoga518@gmail.com>
2024-09-06 14:02:39 +0800

    14

--- YKL14 Decode the Mad man.cpp
+++ YKL14 Decode the Mad man.cpp
@@ -1,25 +1,30 @@
-#include<iostream>
-#include<cctype>
+#include <cctype>
+#include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
 
-    string str;
-    string table = "`1234567890-=qwertyuiop[]]\\asdfghjkl;'zxcvbnm,./";
+	string str;
+	string table = "`1234567890-=qwertyuiop[]]\\asdfghjkl;'zxcvbnm,./";
 
-    while(getline(cin,str)){
-		 for (int i=0; i <str.length(); i++){
-		    	str[i] = tolower(str[i]);
-		 }
-    	 for(int i = 0; i < str.length(); i++){
-	        if(str[i]  == ' '){
-	            cout << " ";
-	        }
-	        else{
-	            cout << table[table.find(str[i]) - 2];
-	        }
-    	}
-    	cout<<endl;
-    }
-
+	while (getline(cin, str))
+	{
+		for (int i = 0; i < str.length(); i++)
+		{
+			str[i] = tolower(str[i]);
+		}
+		for (int i = 0; i < str.length(); i++)
+		{
+			if (str[i] == ' ')
+			{
+				cout << " ";
+			}
+			else
+			{
+				cout << table[table.find(str[i]) - 2];
+			}
+		}
+		cout << endl;
+	}
 }
