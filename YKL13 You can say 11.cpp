f8c526a602553847e1e135165d51bcf4c2177ad4
AdamWang518 <dodoga518@gmail.com>
2024-09-06 13:00:12 +0800

    13

---
+++ YKL13 You can say 11.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main() {
+    string number;
+
+    while (cin >> number) {
+        if (number == "0") {
+        	break;
+        };  
+        int alternatingSum = 0;
+		for(int a=0;a<number.length();a++){
+			if(a%2==0){
+				alternatingSum+=number[a]-'0';
+			}else{
+				alternatingSum-=number[a]-'0';
+			}
+			//seperate odd and even using +-
+		}
+        
+        if (alternatingSum % 11 == 0) {
+            cout << number << " is a multiple of 11." << endl;
+        } else {
+            cout << number << " is not a multiple of 11." << endl;
+        }
+    }
+
+    return 0;
+}
