bedae6c49da760e053ae84e18325856dc0ad9dc3
AdamWang518 <dodoga518@gmail.com>
2024-09-06 12:42:44 +0800

    12

---
+++ YKL12 The Hotel with Infinite Rooms .cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    long long s, d;
+    while (cin >> s >> d) {
+        long long totalDays=0;
+        long long size=s;
+        while(totalDays<d){
+        	//total = S day + (S+1) days (S+2) days
+        	totalDays+=size;
+        	if(totalDays>=d){
+        		break;
+        	}
+        	size++;
+        }
+        cout<<size<<endl;
+    }
+}
