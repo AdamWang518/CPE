2a89199d38be4ec07e77c4e9aa1540e90b73b26d
AdamWang518 <dodoga518@gmail.com>
2024-09-09 10:59:38 +0800

    15

---
+++ YKL15 Common Permutation.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+
+int main() {
+    string a, b;
+    while (cin>>a>>b) {
+        int freqA[26] = {0}, freqB[26] = {0};
+        for (char c : a) {
+            freqA[c - 'a']++;
+        }
+        for (char c : b) {
+            freqB[c - 'a']++;
+        }
+        string result = "";
+        for (int i = 0; i < 26; ++i) {
+            int commonCount = min(freqA[i], freqB[i]);
+            result += string(commonCount, 'a' + i);
+        }
+        cout << result << endl;
+    }
+    
+    return 0;
+}
