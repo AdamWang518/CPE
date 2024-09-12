cbe4ec2921c817352ef68f8d3ebdb71eea2c08b9
AdamWang518 <dodoga518@gmail.com>
2024-09-12 20:37:19 +0800

    Update YKL24 Minesweeper.cpp

--- YKL24 Minesweeper.cpp
+++ YKL24 Minesweeper.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main()
 {
 	int n, m;
-	int fieldCount = 1; // 用来标记是第几个输出场景
+	int fieldCount = 1;
 	while (cin >> n >> m)
 	{
 		if (n == 0 && m == 0)
@@ -14,10 +14,8 @@ int main()
 			break;
 		}
 
-		// 创建一个n行m列的矩阵，并初始化为0
 		vector<vector<int>> v(n, vector<int>(m, 0));
 
-		// 输入地雷信息
 		for (int i = 0; i < n; i++)
 		{
 			for (int j = 0; j < m; j++)
@@ -26,21 +24,19 @@ int main()
 				cin >> a;
 				if (a == '*')
 				{
-					v[i][j] = -1; // -1 表示地雷
+					v[i][j] = -1;
 				}
 			}
 		}
 
-		// 计算每个格子周围的雷数
 		for (int i = 0; i < n; i++)
 		{
 			for (int j = 0; j < m; j++)
 			{
 				if (v[i][j] == -1)
-				{ // 如果当前格子是雷，跳过
+				{
 					continue;
 				}
-				// 检查周围的8个方向
 				for (int x = -1; x <= 1; x++)
 				{
 					for (int y = -1; y <= 1; y++)
@@ -56,10 +52,9 @@ int main()
 			}
 		}
 
-		// 输出结果
 		if (fieldCount > 1)
 		{
-			cout << endl; // 每个场景之间需要一个空行
+			cout << endl;
 		}
 		cout << "Field #" << fieldCount++ << ":" << endl;
 
