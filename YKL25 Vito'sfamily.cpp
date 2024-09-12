ee8d65b504102396c5b31b7373baf9e7c47bf92d
AdamWang518 <dodoga518@gmail.com>
2024-09-12 21:33:23 +0800

    25

---
+++ YKL25 Vito'sfamily.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main(){
+	int a;
+	cin>>a;
+	for(int i=0;i<a;i++){
+		int b;
+		cin>>b;
+		vector<int> v;
+		for(int j=0;j<b;j++){
+			int temp;
+			cin>>temp;
+			v.push_back(temp);
+		}
+		sort(v.begin(),v.end());
+		int vitoHome=v[(int)(v.size()-1)/2];
+		int total=0;
+		for(int j=0;j<b;j++){
+			total+=abs(v[j]-vitoHome);
+		}
+		cout<<total<<endl;
+	}
+	return 0;
+}
