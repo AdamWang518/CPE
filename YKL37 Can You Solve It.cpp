9e854676f6325a4347c8709fc0ff3e55a4847936
AdamWang518 <dodoga518@gmail.com>
2024-09-19 17:13:25 +0800

    38

--- YKL37 Can You Solve It.cpp
+++ YKL37 Can You Solve It.cpp
@@ -8,7 +8,12 @@ int main()
     for (int test = 0; test < t; test++)
     {
         cin >> x1 >> y1 >> x2 >> y2;
-        cout << "Case " << test + 1 << ":";
+        int layer1 = x1 + y1;
+        int layer2 = x2 + y2;
+        int step1 = (layer1 * (layer1 + 1)) / 2 + x1;
+        int step2 = (layer2 * (layer2 + 1)) / 2 + x2;
+        cout << "Case " << test + 1 << ": " << step2 - step1 << endl;
+        ;
     }
     return 0;
 }
--- YKL38 What is the Probability.cpp
+++ YKL38 What is the Probability.cpp
@@ -1,11 +1,29 @@
-#include<iostream>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
 using namespace std;
-int main(){
-	int s,n,i;
+
+int main()
+{
+	int s, n, i;
 	double p;
-	cin>>s;
-	for(int i=0;i<s;i++){
-		cin>>
+	cin >> s;
+
+	for (int j = 0; j < s; j++)
+	{
+		cin >> n >> p >> i;
+
+		if (p == 0)
+		{
+			cout << fixed << setprecision(4) << 0.0000 << endl;
+			continue;
+		}
+
+		double p_lose = 1.0 - p;
+		double p_win_i = (pow(p_lose, i - 1) * p) / (1.0 - pow(p_lose, n));
+
+		cout << fixed << setprecision(4) << p_win_i << endl;
 	}
+
 	return 0;
 }
