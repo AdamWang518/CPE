9d0b23ef1b3b00e349e608e16bc570aaf351c843
AdamWang518 <dodoga518@gmail.com>
2024-09-19 11:54:08 +0800

    35

--- YKL35 Symmetric Matrix.cpp
+++ YKL35 Symmetric Matrix.cpp
@@ -1,11 +1,21 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
-int main(){
-	int a,n;
-	while(cin>>a){
+int main()
+{
+	int a, n, count = 1;
+	while (cin >> a)
+	{
+		cout << "Test #" << count << ": ";
+		count++;
 		vector<vector<int>> v(n, vector<int>(n, 0));
-		
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < n; j++)
+			{
+				cin >> v[i][j];
+			}
+		}
 	}
 	return 0;
 }
