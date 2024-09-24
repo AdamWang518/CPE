deec670ae7dca4374fc459094f31e9fbdf57dc59
AdamWang518 <dodoga518@gmail.com>
2024-09-24 19:13:19 +0800

    41

---
+++ YKL41.cpp
@@ -0,0 +1,23 @@
+#include <bitset>
+#include <iostream>
+
+using namespace std;
+
+int count_ones_in_binary(int number)
+{
+    bitset<32> binary_representation(number);
+    return binary_representation.count();
+}
+
+int main()
+{
+    int n, m;
+    cin >> n;
+    while (n--)
+    {
+        int b1 = count_ones_in_binary(m);
+        int b2 = count_ones_in_binary(stoi(to_string(m), nullptr, 16));
+        cout << b1 << " " << b2 << endl;
+    }
+    return 0;
+}
